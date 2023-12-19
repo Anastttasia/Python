de64e338df05b94219518c659bd6bdd680ff2c03
Anastttasia <nastya.kaneva.99@gmail.com>
2023-12-19 23:34:35 +0300

    Add read, write file

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
-try:
-    with open("purchase_log.txt", "r", encoding="utf-8", errors= None) as file:
-        dict_file = {}
+import csv
 
-        for line in file:
-            data = line
-            print(line)
-            data = data.rstrip().lstrip('{').rstrip('}')
-            data_pair = data.split(",")
-            left_pair = data_pair[0].split(":")
-            key_left_pair = left_pair[0].lstrip('"').rstrip('"')
-            value_left_pair = left_pair[1].lstrip(' "').rstrip('"')
+class Clients:
+    type_decice = {
+        'tablet':'планшета',
+        'desktop':'персонального компьютера',
+        'laptop':'ноутбука',
+        'mobile':'мобильного устройства'
+    }
+    gender_type = {
+        'female':'женского пола',
+        'male':'мужского пола'
+    }
 
-            data = data.rstrip().lstrip('{').rstrip('}')
-            right_pair = data_pair[1].split(":")
-            key_right_pair = right_pair[0].lstrip(' "').rstrip('"')
-            value_right_pair = right_pair[1].lstrip(' "').rstrip('"')
-            dict_file.update({value_left_pair: value_right_pair})
+    def __init__(self, name, gender, age, device, browser, summ, region):
+        self.name = name
+        self.gender = Clients.gender_type.get(gender)
+        self.age = age
+        self.device = Clients.type_decice.get(device)
+        self.browser = browser
+        self.summ = summ
+        self.region = region
 
-        print("dict_file")
-        print(dict_file)
+    
+    def get_template_row(self):
+        return f'Пользователь {self.name} {self.gender},{self.age} лет совершил покупку на {self.summ} у.е. с {self.device} c браузера {self.browser}. Регион, из которого совершалась покупка: {self.region}'
 
-except Exception as ex:
-    print('Error')
+with open('clients.txt', 'w') as output_file:
+    with open("web_clients_correct.csv") as input_file:
+        for p_data in csv.reader(input_file):
+            print("ROW")
+            print(p_data)
+            output_file.write(Clients(p_data[0], p_data[3], p_data[4], p_data[1], p_data[2], p_data[5], p_data[6]).get_template_row() + '\n')
